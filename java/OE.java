a1150cadfcb613c1d7d312fdbdee7496c8da5812
ShabinaAslah <shabinashan16@gmail.com>
2026-02-25 09:07:33 +0530

    Add files via upload

---
+++ java/OE.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+class OE {
+	public static void main (String[]args){
+		int num=6;
+		
+		
+		if (num%2==0){
+			System.out.println(num+"is Even");
+		}
+		
+		else{
+			System.out.println(num+"is Odd");
+		}
+	}
+}
+			
---
+++ java/Product.java
@@ -0,0 +1,46 @@
+public class Product{
+	String pname,pcode;
+	int price;
+	public Product(){}
+		public Product(String pname,String pcode,int price){
+			this.pname=pname;
+			this.pcode=pcode;
+			this.price=price;}
+		public void setPname(String pname){
+		  this.pname=pname;}
+		public String getPnane(){
+		  return pname;}
+		public void setPcode(String pcode){
+		  this.pcode=pcode;}
+		public int getPrice(){
+		  return price;}
+		public void setPrice(int price){
+		  this.price=price;}
+		public void display(){
+		  System.out.println("pcode:"+this.pcode);
+		  System.out.println("pname:"+this.pname);
+		  System.out.println("pricde:"+this.price+"\n");}
+		}
+
+class Cars{
+  public static void main(String[] args){
+    Product p1=new Product();
+    p1.pcode="car123";
+    p1.pname="benz";
+    p1.price=100000;
+    System.out.println("Displaying p1:");
+    p1.display();
+    
+    Product p2=new Product("jaguar","car456",250000);
+    System.out.println("Displaying p2:");
+    p2.display();
+    
+    Product p3=new Product("maruthi","carbee",500000);
+    System.out.println("Displaying p3:");
+    p3.display();
+    
+    Product p=p3.getPrice()<(p1.price<p2.price? p1.price:p2.price)?p3:(p1.price<p2.price?p1:p2);
+    System.out.println("\n display product with lowest price");
+    p.display();
+  }
+}
